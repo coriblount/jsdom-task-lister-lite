71509991c36974006c9352902f80a3f654ee72ce
Cori Blount <heycorihey@icloud.com>
2021-04-06 14:39:49 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+// initialize task list
+let taskList = newTask();
+//grab DOM elements as needed
+//form input fields
+let newTaskForm = document.getElementById("create-task-form")
+let newTaskDescription = document.getElementById("new-task-description")
+let newTaskPriority = document.getElementById("new-task-priority")
+
+//ul where task will live on dom
+
+//attach event listeners
+
 });
