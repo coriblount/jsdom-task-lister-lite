9593138d345374df546fe82c4a622172005f8849
Cori Blount <heycorihey@icloud.com>
2021-04-06 15:26:15 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -4,12 +4,30 @@ document.addEventListener("DOMContentLoaded", () => {
 let taskList = newTask();
 //grab DOM elements as needed
 //form input fields
-let newTaskForm = document.getElementById("create-task-form")
-let newTaskDescription = document.getElementById("new-task-description")
-let newTaskPriority = document.getElementById("new-task-priority")
+let newTaskForm = document.getElementById("create-task-form");
+let newTaskDescription = document.getElementById("new-task-description");
+let newTaskPriority = document.getElementById("new-task-priority");
 
-//ul where task will live on dom
+//list where task will live on dom
+let taskUl = document.getElementById("tasks");
 
+let renderApp = () => (taskUl.innerHTML = taskList.renderTasks());
 //attach event listeners
 
+
+
+newTaskForm.addEventListener("submit", (e)) {
+  e.preventDefault();
+  taskList.createNewTask(newTaskDescription.value);
+  // reset form
+  e.target.reset();
+  renderApp();
 });
+
+taskUl.addEventListener("click", (e) => {
+  if (e.target.nodeName === "BUTTON") {
+    taskList.deleteTask(e.target.dataset.description);
+    renderApp();
+  }
+ });
+}); 
